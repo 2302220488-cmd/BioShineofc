bf765b1c84f9779ac5514e283793c94e352f8960
2302220488-cmd <2302220488@students.satyaterrabhinneka.ac.id>
2026-01-06 16:06:58 +0700

    suren.js

--- suren.js
+++ suren.js
@@ -1,21 +1,25 @@
 const images = [
-    "suren01.JPG",
-    "suren02.JPG",
-    "suren03.JPG",
+    "suren04.JPG",
+    "suren05.JPG",
+    "suren09.JPG",
     "suren07.JPG",
-    "suren09.JPG"
+    "suren08.JPG"
 ];
 
 let index = 0;
 const sliderImage = document.getElementById("sliderImage");
 
-// CEK dulu, kalau slider ada baru jalan
-if (sliderImage) {
-    setInterval(() => {
-        index++;
-        if (index >= images.length) {
-            index = 0;
-        }
-        sliderImage.src = images[index];
-    }, 3000);
+function nextSlide() {
+    index++;
+    if(index >= images.length) index = 0;
+    sliderImage.src = images[index];
 }
+
+function prevSlide() {
+    index--;
+    if(index < 0) index = images.length - 1;
+    sliderImage.src = images[index];
+}
+
+// Auto slide setiap 3 detik
+setInterval(nextSlide, 3000);
