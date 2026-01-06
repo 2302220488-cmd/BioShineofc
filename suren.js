9affb428d224dc08bb843df2ff682ecfb4fefc0d
2302220488-cmd <2302220488@students.satyaterrabhinneka.ac.id>
2026-01-06 16:18:29 +0700

    suren.js

--- suren.js
+++ suren.js
@@ -6,6 +6,15 @@ const images = [
     "suren08.JPG"
 ];
 
+const contactForm = document.getElementById('contactForm');
+const formMsg = document.getElementById('formMsg');
+
+contactForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    formMsg.textContent = "Thank you! Your message has been sent.";
+    contactForm.reset();
+});
+
 let index = 0;
 const sliderImage = document.getElementById("sliderImage");
 
@@ -23,3 +32,4 @@ function prevSlide() {
 
 // Auto slide setiap 3 detik
 setInterval(nextSlide, 3000);
+
