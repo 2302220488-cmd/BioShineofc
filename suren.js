5f51bf2d112c3411cc8993367dfdd0f8f4093de9
2302220488-cmd <2302220488@students.satyaterrabhinneka.ac.id>
2026-01-06 16:25:37 +0700

    suren.js

--- suren.js
+++ suren.js
@@ -1,3 +1,6 @@
+/* ==========================
+   SLIDER ABOUT-US
+========================== */
 const images = [
     "suren04.JPG",
     "suren05.JPG",
@@ -6,24 +9,17 @@ const images = [
     "suren08.JPG"
 ];
 
-const contactForm = document.getElementById('contactForm');
-const formMsg = document.getElementById('formMsg');
-
-contactForm.addEventListener('submit', function(e){
-    e.preventDefault();
-    formMsg.textContent = "Thank you! Your message has been sent.";
-    contactForm.reset();
-});
-
 let index = 0;
 const sliderImage = document.getElementById("sliderImage");
 
+// Ganti slide ke kanan
 function nextSlide() {
     index++;
     if(index >= images.length) index = 0;
     sliderImage.src = images[index];
 }
 
+// Ganti slide ke kiri
 function prevSlide() {
     index--;
     if(index < 0) index = images.length - 1;
@@ -33,3 +29,16 @@ function prevSlide() {
 // Auto slide setiap 3 detik
 setInterval(nextSlide, 3000);
 
+/* ==========================
+   CONTACT FORM
+========================== */
+const contactForm = document.getElementById('contactForm');
+const formMsg = document.getElementById('formMsg');
+
+if(contactForm){
+    contactForm.addEventListener('submit', function(e){
+        e.preventDefault(); // supaya halaman tidak reload
+        formMsg.textContent = "Thank you! Your message has been sent.";
+        contactForm.reset();
+    });
+}
